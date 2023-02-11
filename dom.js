26acc6fd793e0af1bed77c264611f13e29d9819b
Irem Soyaker <irem_95@windowslive.com>
2023-02-11 13:10:30 +1100

    adding css and js updates

--- dom.js
+++ dom.js
@@ -1,17 +1,21 @@
 // document.getElementById("bio").innerHTML="Mila Soyaker : 2022"
 
-var intro1 = document.getElementById("intro1")
+// var intro1 = document.getElementById("intro1")
 
-var message = document.getElementById("message")
+// var message = document.getElementById("message")
 
-message.innerHTML = intro1.innerHTML
+// message.innerHTML = intro1.innerHTML
 
-var allLists = document.getElementsByTagName("ul")
+// var allLists = document.getElementsByTagName("ul")
 
-var cities = allLists[0]
+// var cities = allLists[0]
 
-allItems = cities.getElementsByTagName("li")
+// allItems = cities.getElementsByTagName("li")
 
-for (let i = 0; i < allItems.length; i++) {
-    alert(allItems[i].innerHTML)
-}
+// for (let i = 0; i < allItems.length; i++) {
+//     alert(allItems[i].innerHTML)
+// }
+
+ var classElement = document.getElementsByClassName("intro1")
+
+ alert(classElement[1].innerHTML)
